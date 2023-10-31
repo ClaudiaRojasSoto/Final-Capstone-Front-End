72076d13a2f2b602cab1707ff39c2b6a3975b62d
Alexandre <alexsales001@hotmail.com>
2023-10-31 16:56:52 -0300

    finish setting reserve

--- src/components/Reserve/Reserve.js
+++ src/components/Reserve/Reserve.js
@@ -15,6 +15,35 @@ function Reserve() {
       city: city
     });
   };
+
+  return (
+    <div>
+      <h2>Reserve an Appointment</h2>
+      <form onSubmit={handleSubmit}>
+        <label>
+          Username:
+          <input type="text" value={username} readOnly />
+        </label>
+        <br />
+        <label>
+          Selected Item:
+          <input type="text" value={selectedItem} readOnly />
+        </label>
+        <br />
+        <label>
+          Date:
+          <input type="date" value={date} onChange={e => setDate(e.target.value)} required />
+        </label>
+        <br />
+        <label>
+          City:
+          <input type="text" value={city} onChange={e => setCity(e.target.value)} required />
+        </label>
+        <br />
+        <button type="submit">Reserve</button>
+      </form>
+    </div>
+  );
 }
 
-
+export default Reserve;
