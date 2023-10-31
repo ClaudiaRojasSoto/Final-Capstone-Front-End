5314bae4910548ec102cf048a7a1cc0132827ecc
Claudia  P. R. Soto <97201255+ClaudiaRojasSoto@users.noreply.github.com>
2023-10-30 22:09:37 -0300

    Add the reservation list to the store

--- src/redux/store.js
+++ src/redux/store.js
@@ -2,12 +2,14 @@ import { configureStore } from '@reduxjs/toolkit';
 import carSlice from './slices/carSlice';
 import loginReducer from './slices/loginSlice';
 import registerReducer from './slices/registerSlice';
+import reservationReducer from './slices/reservationSlice';
 
 const store = configureStore({
   reducer: {
     car: carSlice,
     login: loginReducer,
     register: registerReducer,
+    reservation: reservationReducer,
   },
 });
 
