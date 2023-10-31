39c6eb1426c428f742c6f701e56dd5d59f2e6656
Alexandre <alexsales001@hotmail.com>
2023-10-31 17:05:43 -0300

    made changes to match the design

--- src/components/Reserve/Reserve.js
+++ src/components/Reserve/Reserve.js
@@ -1,49 +1,43 @@
 import React, { useState } from 'react';
+import './reserve.css';
 
 function Reserve() {
-  const [date, setDate] = useState('');
-  const [city, setCity] = useState('');
-  const username = "JohnDoe";
-  const selectedItem = "Vespa XYZ";
-  const handleSubmit = (event) => {
-    event.preventDefault();
+    const [date, setDate] = useState('');
+    const [city, setCity] = useState('');
+    const username = "JohnDoe";
+    const selectedItem = "Vespa XYZ";
 
-    console.log({
-      username: username,
-      selectedItem: selectedItem,
-      date: date,
-      city: city
-    });
-  };
+    const handleSubmit = (event) => {
+        event.preventDefault();
 
-  return (
-    <div>
-      <h2>Reserve an Appointment</h2>
-      <form onSubmit={handleSubmit}>
-        <label>
-          Username:
-          <input type="text" value={username} readOnly />
-        </label>
-        <br />
-        <label>
-          Selected Item:
-          <input type="text" value={selectedItem} readOnly />
-        </label>
-        <br />
-        <label>
-          Date:
-          <input type="date" value={date} onChange={e => setDate(e.target.value)} required />
-        </label>
-        <br />
-        <label>
-          City:
-          <input type="text" value={city} onChange={e => setCity(e.target.value)} required />
-        </label>
-        <br />
-        <button type="submit">Reserve</button>
-      </form>
-    </div>
-  );
+        console.log({
+            username: username,
+            selectedItem: selectedItem,
+            date: date,
+            city: city
+        });
+    };
+
+    return (
+        <div className="reserve-container">
+            <h2 className="reserve-title">BOOK A VESPA TEST-RIDE</h2>
+            <p className="reserve-info">
+                There are 34 different versions of the Vespa. Today five series are in production: the classic manual transmission PX and the modern CVT transmission S, LX, GT, and GTS. We have showrooms all over the globe which some include test-riding facilities.
+                If you wish to find out if a test-ride is available in your area, please use the selector below.
+            </p>
+            <form className="reserve-form" onSubmit={handleSubmit}>
+                <label className="reserve-label">
+                    City:
+                    <select className="reserve-select" value={city} onChange={e => setCity(e.target.value)} required>
+                        {/* You can add more cities here */}
+                        <option value="London">London</option>
+                        <option value="New York">New York</option>
+                    </select>
+                </label>
+                <button className="reserve-btn" type="submit">Book Now</button>
+            </form>
+        </div>
+    );
 }
 
 export default Reserve;
