4422d7b5a4710c9c82c75ba9fe4263f042c27efa
PabloBona <pablobonasera@gmail.com>
2023-10-31 18:37:34 -0300

    Update CarCard Add styles

--- src/components/CardCar/CardCar.js
+++ src/components/CardCar/CardCar.js
@@ -1,11 +1,48 @@
 import React from 'react';
+// eslint-disable-next-line import/no-extraneous-dependencies
+import PropTypes from 'prop-types';
+import {
+  MDBCard,
+  MDBCardImage,
+  MDBCardBody,
+  MDBCardTitle,
+  MDBCardText,
+  MDBCardFooter,
+  MDBRow,
+  MDBCol,
+} from 'mdb-react-ui-kit';
 
-const CardCar = (carImg) => (
-  <div>
-    <a href="./">
-      <img src={carImg} alt="Car 1" />
-    </a>
-  </div>
+const CarCard = ({ cars }) => (
+  <MDBRow className="row-cols-1 row-cols-md-3 g-4">
+    {cars.map((car) => (
+      <MDBCol key={car.id}>
+        <MDBCard className="h-100">
+          <MDBCardImage
+            src={car.image_url}
+            alt={car.name}
+            position="top"
+          />
+          <MDBCardBody>
+            <MDBCardText>{car.description}</MDBCardText>
+          </MDBCardBody>
+          <MDBCardFooter>
+            <MDBCardTitle>{car.name}</MDBCardTitle>
+          </MDBCardFooter>
+        </MDBCard>
+      </MDBCol>
+    ))}
+  </MDBRow>
 );
 
-export default CardCar;
+CarCard.propTypes = {
+  cars: PropTypes.arrayOf(
+    PropTypes.shape({
+      id: PropTypes.number.isRequired,
+      name: PropTypes.string.isRequired,
+      description: PropTypes.string.isRequired,
+      image_url: PropTypes.string.isRequired,
+    }),
+  ).isRequired,
+};
+
+export default CarCard;
--- src/components/Hero/Hero.js
+++ src/components/Hero/Hero.js
@@ -1,16 +1,48 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import CarCard from '../CardCar/CardCar';
 
-const hero = () => (
-  <div>
-    <div className="hero-title text-center">
-      <h2>LATEST MODELS</h2>
-      <p className="text-black-50">Please Select a Car Model</p>
-    </div>
-    <div>
-      <CarCard />
-    </div>
-  </div>
-);
+const Hero = () => {
+  const [carsData, setCarsData] = useState([]);
 
-export default hero;
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await fetch('http://localhost:3000/api/cars', {
+          method: 'GET',
+          headers: {
+            'Content-Type': 'application/json',
+          },
+          credentials: 'include',
+        });
+
+        if (!response.ok) {
+          throw new Error('Error fetching cars');
+        }
+
+        const data = await response.json();
+        setCarsData(data);
+        console.log('Cars Data:', data);
+      } catch (error) {
+        console.error('Error fetching cars:', error);
+      }
+    };
+
+    fetchData();
+  }, []);
+
+  return (
+    <div className="container-fluid d-flex align-items-center justify-content-center vh-100">
+      <div className="text-center">
+        <div className="hero-title">
+          <h2>LATEST MODELS</h2>
+          <p className="text-black-50">Please Select a Car Model</p>
+        </div>
+        <div className="mt-5">
+          <CarCard cars={carsData} />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Hero;
