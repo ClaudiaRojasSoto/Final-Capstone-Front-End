fda97bc7794ed990d73c7eaea82cef829c0329ca
Claudia  P. R. Soto <97201255+ClaudiaRojasSoto@users.noreply.github.com>
2023-11-01 20:45:32 -0300

    Fix linter errors

--- src/components/Sidebar/Sidebar.js
+++ src/components/Sidebar/Sidebar.js
@@ -1,16 +1,16 @@
 import React from 'react';
-import { useDispatch } from 'react-redux'; // Añadido
+import { useDispatch } from 'react-redux';
 import { Link } from 'react-router-dom';
-import { toggleDeleteMode } from '../../redux/slices/carSlice'; // Añadido
+import { toggleDeleteMode } from '../../redux/slices/carSlice';
 import './sidebar.css';
 import carRental from './carRental.png';
 import 'boxicons';
 
 const Sidebar = () => {
-  const dispatch = useDispatch(); // Añadido
+  const dispatch = useDispatch();
 
   const handleDeleteClick = () => {
-    dispatch(toggleDeleteMode()); // Añadido
+    dispatch(toggleDeleteMode());
   };
 
   return (
@@ -20,33 +20,33 @@ const Sidebar = () => {
       </div>
       <ul className="flex-column mt-3 nav custom-size">
         <li className="nav-item">
-          <a className="nav-link active text-black" aria-current="page" href="#Models">RESERVE MODEL</a>
+          <a className="nav-link active text-black" aria-current="page" aria-label="Reserve Model" href="#Models">RESERVE MODEL</a>
         </li>
         <li className="nav-item">
-          <a className="nav-link text-black" href="/user_reservations">MY RESERVATIONS</a>
+          <a className="nav-link text-black" aria-label="My Reservations" href="/user_reservations">MY RESERVATIONS</a>
         </li>
         <li className="nav-item">
-          <Link to="/addcar" className="nav-link active text-black">ADD CAR</Link>
+          <Link to="/addcar" className="nav-link active text-black" aria-label="Add Car">ADD CAR</Link>
         </li>
         <li className="nav-item">
-          <button type="button" onClick={handleDeleteClick} className="nav-link text-black">DELETE CAR</button>
-          {/* Botón añadido */}
+          <button type="button" onClick={handleDeleteClick} className="nav-link text-black" aria-label="Delete Car">DELETE CAR</button>
         </li>
       </ul>
       <div className="align-items-end d-flex justify-content-evenly mb-3 custom-grow">
-        <a href="enlace-de-twitter" target="_blank" rel="noopener noreferrer">
+        {/* aria-labels added below */}
+        <a href="enlace-de-twitter" target="_blank" rel="noopener noreferrer" aria-label="Twitter">
           <box-icon type="logo" name="twitter" />
         </a>
-        <a href="enlace-de-facebook" target="_blank" rel="noopener noreferrer">
+        <a href="enlace-de-facebook" target="_blank" rel="noopener noreferrer" aria-label="Facebook">
           <box-icon type="logo" name="facebook" />
         </a>
-        <a href="enlace-de-google" target="_blank" rel="noopener noreferrer">
+        <a href="enlace-de-google" target="_blank" rel="noopener noreferrer" aria-label="Google">
           <box-icon type="logo" name="google" />
         </a>
-        <a href="enlace-de-vimeo" target="_blank" rel="noopener noreferrer">
+        <a href="enlace-de-vimeo" target="_blank" rel="noopener noreferrer" aria-label="Vimeo">
           <box-icon type="logo" name="vimeo" />
         </a>
-        <a href="enlace-de-pinterest" target="_blank" rel="noopener noreferrer">
+        <a href="enlace-de-pinterest" target="_blank" rel="noopener noreferrer" aria-label="Pinterest">
           <box-icon type="logo" name="pinterest-alt" />
         </a>
       </div>
