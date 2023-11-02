c22ef51503afe7c4d9aea65a5abb85d9e8a38f06
Alexandre <alexsales001@hotmail.com>
2023-11-02 13:56:52 -0300

    added reserve route to app.js and import

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import Signup from './components/session/SignUp';
 import './app.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import CarForm from './components/CarForm/CarForm';
+import Reserve from './components/Reserve/reserve';
 
 function App() {
   return (
@@ -17,7 +18,7 @@ function App() {
           <Route path="/signup" element={<Signup />} />
           <Route path="/home" element={<Main />} />
           <Route path="/addcar" element={<CarForm />} />
-          <Route path="/reserve" component={Reserve} />
+          <Route path="/reserve" element={<Reserve />} />
         </Routes>
       </Router>
     </div>
