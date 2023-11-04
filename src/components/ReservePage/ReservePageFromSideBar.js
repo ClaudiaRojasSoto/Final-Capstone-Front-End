c9680c05181725d544f2475e6c60f97391f1e7ac
PabloBona <pablobonasera@gmail.com>
2023-11-04 09:37:18 -0300

    Delete unused css file in reserve component

--- src/components/ReservePage/ReservePageFromSideBar.js
+++ src/components/ReservePage/ReservePageFromSideBar.js
@@ -2,7 +2,6 @@ import React, { useState, useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { useNavigate } from 'react-router-dom';
 import { getCurrentUser } from '../../redux/slices/userSlice';
-import './reserve.css';
 
 const ReservePageFromSideBar = () => {
   const dispatch = useDispatch();
